3d3909eb5d2f65dc1180d27a9aa9052f3312924f
SamarSaeed1146 <samarsaeed374@gmail.com>
2024-08-22 12:50:04 +0500

    Accordion In Shadcn Ui

---
+++ one/src/app/accordion.tsx
@@ -0,0 +1,19 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+export function AccordionDemo() {
+  return (
+    <Accordion type="single" collapsible className="w-full">
+      <AccordionItem value="item-1">
+        <AccordionTrigger className="hover:text-blue-500 duration-500">Is it accessible?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It adheres to the WAI-ARIA design pattern.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+}
