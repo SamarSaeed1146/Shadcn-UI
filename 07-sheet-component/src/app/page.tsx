cf9b82915db13ab8c64ff8b8c64b5cd7a7726926
SamarSaeed1146 <samarsaeed374@gmail.com>
2024-08-23 17:04:56 +0500

    Sheet Component In Shadcn Ui

---
+++ 07-sheet-component/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { SheetDemo } from "./Sheet";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-center pt-5">Shadcn Ui Sheet Component</h1>
+      <div>
+        <SheetDemo />
+      </div>
+    </div>
+  );
+}
